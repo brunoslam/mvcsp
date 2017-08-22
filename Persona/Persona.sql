702d6429c059c45ee7a7562239113b691ca5c8e9
Bruno Alonso Palma Ávila <bpalma@latinshare.com>
2017-08-22 10:10:58 -0300

    asd

---
+++ Persona/Persona.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Persona]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre] VARCHAR(50) NULL, 
+    [Rut] VARCHAR(50) NULL, 
+    [Comuna] VARCHAR(50) NULL, 
+    [EsPersona] BIT NULL, 
+    [Direccion] VARBINARY(50) NULL
+)
