0b1dee03f354241cb6e9ca86437607a98a452903
Bruno Alonso Palma Ávila <bpalma@latinshare.com>
2017-08-23 15:58:21 -0300

    MVC

---
+++ DemoCSOMWeb/Controllers/SharePointController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoCSOMWeb.Controllers
+{
+    public class SharePointController
+    {
+
+
+        public void InsertarElemento()
+        {
+
+        }
+
+
+        public void ObtenerElemento()
+        {
+
+        }
+
+        public void EliminarElemento()
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
